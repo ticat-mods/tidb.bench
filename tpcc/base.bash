d6067a6b19da9e50cc91ca293732b89c30d2e6bd
Liu Cong <innerr@gmail.com>
2021-07-04 15:38:52 +0800

    wip on bench meta
    Signed-off-by: Liu Cong <innerr@gmail.com>

--- tpcc/base.bash
+++ tpcc/base.bash
@@ -9,16 +9,10 @@ bin="tiup bench tpcc"
 function env_val()
 {
 	local key="${1}"
-	val=`echo "${env}" | { grep "^${key}" || test $? = 1; } | awk '{print $2}'`
+	local val=`echo "${env}" | { grep "^${key}" || test $? = 1; } | awk '{print $2}'`
 	if [ -z "${val}" ]; then
 		echo "[:-] no env val '${key}'" >&2
 		exit 1
 	fi
 	echo "${val}"
 }
-
-host=`env_val 'mysql.host'`
-port=`env_val 'mysql.port'`
-user=`env_val 'mysql.user'`
-
-warehouses=`env_val 'bench.tpcc.warehouses'`
--- tpcc/load.bash
+++ tpcc/load.bash
@@ -6,6 +6,12 @@ shift
 here=`cd $(dirname ${BASH_SOURCE[0]}) && pwd`
 . "${here}/base.bash" "${env}" "${here}"
 
+host=`env_val 'mysql.host'`
+port=`env_val 'mysql.port'`
+user=`env_val 'mysql.user'`
+
+warehouses=`env_val 'bench.tpcc.warehouses'`
+
 threads=`env_val 'bench.tpcc.load.threads'`
 
 ${bin} \
