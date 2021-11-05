90565ab37b02313914905d938c014b53aa26d9f7
p4tr1ck <patrick.li@pingcap.com>
2021-11-05 17:52:15 +0800

    add helper parse_sysbench_detail

--- helper/helper.bash
+++ helper/helper.bash
@@ -1,4 +1,5 @@
 . "`cd $(dirname ${BASH_SOURCE[0]}) && pwd`/ticat.helper.bash/helper.bash"
+. "`cd $(dirname ${BASH_SOURCE[0]}) && pwd`/tiup.helper.bash/helper.bash"
 
 function parse_tpmc()
 {
@@ -24,6 +25,47 @@ function parse_sysbench_events()
 {
 	local log="${1}"
 	cat "${log}" | grep "total number of events" | awk -F 'events: ' '{print $2}' | awk '{print $1}'
+}
+
+function parse_sysbench_detail()
+{
+    local log="${1}"
+    cat "${log}" | awk '
+    BEGIN {
+        state = ""
+        num_queries = 0
+        num_txns = 0
+        total_times = 1
+        min = 0
+        avg = 0
+        max = 0 
+        p95 = 0
+    }
+    /^SQL statistics/ { state = "sql" }
+    /^General statistics/ { state = "time" }
+    /^Latency/ { state = "latency" }
+    state == "sql" {
+        switch ($1) {
+        case /transactions/: num_txns=$2; break;
+        case /queries/: num_queries=$2; break;
+        }
+    }
+    state == "time" && /total time/ {
+        total_times=substr($3, 0, length($3)-1)
+    }
+    state == "latency" {
+        switch ($1) {
+        case /min/: min=$2; break;
+        case /avg/: avg=$2; break;
+        case /max/: max=$2; break;
+        case /95th/: p95=$3; break;
+        }
+    }
+    END {
+        printf "qps tps min avg p95 max\n"
+        printf "%.2f %.2f %s %s %s %s\n", num_queries/total_times,num_txns/total_times, min, avg, p95, max
+    }
+    '
 }
 
 function parse_ycsb()
