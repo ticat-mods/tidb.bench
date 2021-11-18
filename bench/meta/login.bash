1806cf21b6c935569427d389635d929db4f06756
Liu Cong <innerr@gmail.com>
2021-11-18 22:54:02 +0800

    better login command for meta db
    Signed-off-by: Liu Cong <innerr@gmail.com>

---
+++ bench/meta/login.bash
@@ -0,0 +1,12 @@
+set -euo pipefail
+. "`cd $(dirname ${BASH_SOURCE[0]}) && pwd`/../../helper/helper.bash"
+
+env=`cat ${1}/env`
+shift
+
+host=`must_env_val "${env}" 'bench.meta.host'`
+port=`must_env_val "${env}" 'bench.meta.port'`
+user=`must_env_val "${env}" 'bench.meta.user'`
+db=`must_env_val "${env}" 'bench.meta.db-name'`
+
+mysql -h "${host}" -P "${port}" -u "${user}" -D "${db}"
