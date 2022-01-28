27bb71ad3d7055604e580780e30a388ea9e36c58
Liu Cong <innerr@gmail.com>
2022-01-29 01:08:21 +0800

    fix helper importing
    Signed-off-by: Liu Cong <innerr@gmail.com>

--- helper/helper.bash
+++ helper/helper.bash
@@ -1,6 +1,12 @@
 . "`cd $(dirname ${BASH_SOURCE[0]}) && pwd`/bash.helper/helper.bash"
 . "`cd $(dirname ${BASH_SOURCE[0]}) && pwd`/tiup.helper/tiup.bash"
 
+# TODO: let each workload import their own helper
+. "`cd $(dirname ${BASH_SOURCE[0]}) && pwd`/tpcc.bash"
+. "`cd $(dirname ${BASH_SOURCE[0]}) && pwd`/tpch.bash"
+. "`cd $(dirname ${BASH_SOURCE[0]}) && pwd`/sysbench.bash"
+. "`cd $(dirname ${BASH_SOURCE[0]}) && pwd`/ycsb.bash"
+
 function timestamp()
 {
 	echo `date +%s`
