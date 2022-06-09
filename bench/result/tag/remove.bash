e36af57dac65d13db7183f8e2ba20628f5040297
Liu Cong <innerr@gmail.com>
2022-06-09 19:44:29 +0800

    support db pwd in `bench.result.tag.add|remove`
    Signed-off-by: Liu Cong <innerr@gmail.com>

--- bench/result/tag/remove.bash
+++ bench/result/tag/remove.bash
@@ -11,7 +11,8 @@ tags="${2}"
 host=`must_env_val "${env}" 'bench.meta.host'`
 port=`must_env_val "${env}" 'bench.meta.port'`
 user=`must_env_val "${env}" 'bench.meta.user'`
+pp=`env_val "${env}" 'bench.meta.pwd'`
 db=`must_env_val "${env}" 'bench.meta.db-name'`
 
-bench_record_rm_tags "${host}" "${port}" "${user}" "${db}" "${ids}" "${tags}"
+bench_record_rm_tags "${host}" "${port}" "${user}" "${pp}" "${db}" "${ids}" "${tags}"
 echo "[:)] done"
