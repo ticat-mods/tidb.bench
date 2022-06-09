544eb109e5997ee5ba3f8b1e46baf0ed18bcd01f
Liu Cong <innerr@gmail.com>
2022-06-09 22:36:29 +0800

    add cmd `bench.result.move-to-db`
    Signed-off-by: Liu Cong <innerr@gmail.com>

---
+++ meta/db/move-to-db.bash
@@ -0,0 +1,24 @@
+set -euo pipefail
+. "`cd $(dirname ${BASH_SOURCE[0]}) && pwd`/../../helper/helper.bash"
+
+env=`cat ${1}/env`
+shift
+
+host=`must_env_val "${env}" 'bench.meta.host'`
+port=`must_env_val "${env}" 'bench.meta.port'`
+user=`must_env_val "${env}" 'bench.meta.user'`
+pp=`env_val "${env}" 'bench.meta.pwd'`
+shift 4
+
+src="${1}"
+dest="${2}"
+
+my_ensure_db "${host}" "${port}" "${user}" "${pp}" "${src}"
+my_ensure_db "${host}" "${port}" "${user}" "${pp}" "${dest}"
+
+tables=('bench_meta' 'bench_tags' 'bench_data')
+for table in "${tables[@]}"; do
+	my_exe "${host}" "${port}" "${user}" "${pp}" "${dest}" "ALTER TABLE ${src}.${table} RENAME ${dest}.${table}"
+done
+
+echo "[:)] meta data moved from '${src}' to '${dest}'"
